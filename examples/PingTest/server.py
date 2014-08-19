08b8fe216b43a4c22e546a9bee276322dedc3764
Bořek Patzák <borek.patzak@gmail.com>
2014-08-19 16:06:49 +0200

    Adding ping test sources

---
+++ examples/PingTest/server.py
@@ -0,0 +1,58 @@
+import sys
+sys.path.append('../..')
+
+from mupif import Application
+from mupif import TimeStep
+from mupif import APIError
+from mupif import PropertyID
+from mupif import Property
+from mupif import ValueType
+import Pyro4
+
+# required firewall settings (on ubuntu):
+# for computer running daemon (this script)
+# sudo iptables -A INPUT -p tcp -d 0/0 -s 0/0 --dport 44382 -j ACCEPT
+# for computer running a nameserver
+# sudo iptables -A INPUT -p tcp -d 0/0 -s 0/0 --dport 9090 -j ACCEPT
+
+
+class PingServerApplication(Application.Application):
+    """
+    Simple application that computes an arithmetical average of mapped property
+    """
+    def __init__(self, file):
+        self.value = 0.0
+        self.count = 0.0
+        self.contrib = 0.0
+    def getProperty(self, propID, time, objectID=0):
+        if (propID == PropertyID.PID_CumulativeConcentration):
+            return Property.Property(float(self.value)/self.count, PropertyID.PID_CumulativeConcentration, ValueType.Scalar, time, None, 0)
+        else:
+            raise APIError.APIError ('Unknown property ID')
+    def setProperty(self, property, objectID=0):
+        if (property.getPropertID() == PropertyID.PID_Concentration):
+            # remember the mapped value
+            self.contrib = property.getValue()
+        else:
+            raise APIError.APIError ('Unknown property ID')
+    def solveStep(self, tstep, stageID=0, runInBackground=False):
+        # here we actually accumulate the value using value of mapped property
+        self.value=self.value+self.contrib
+        self.count = self.count+1
+
+    def getCriticalTimeStep(self):
+        return 1.0
+
+    def getApplicationSignature(self):
+        return "CTU Ping server, version 1.0"
+
+
+daemon = Pyro4.Daemon(host='mech.fsv.cvut.cz', port=44382, nathost="localhost", natport=5555)
+ns     = Pyro4.locateNS(host='147.32.130.137', port=9090)
+
+app2 = PingServerApplication("/dev/null")
+#register agent
+uri    = daemon.register(app2)
+ns.register("Mupif.PingServerApplication", uri)
+print uri
+daemon.requestLoop()
---
+++ examples/PingTest/test.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('../..')
+
+from mupif import Application
+from mupif import TimeStep
+from mupif import APIError
+from mupif import PropertyID
+from mupif import Property
+from mupif import ValueType
+import Pyro4
+
+time  = 0
+dt    = 1
+expectedValue = 4.5
+
+
+daemon = Pyro4.Daemon()
+ns     = Pyro4.locateNS('mech.fsv.cvut.cz', 9090)
+
+# locate remote PingServer application, request remote proxy
+uri = ns.lookup("Mupif.PingServerApplication")
+print uri
+serverApp = Pyro4.Proxy(uri)
+
+#app2.__init__(None)
+
+
+print "Connected to ", serverApp.getApplicationSignature()
+print "Generating test sequence ...",
+
+for i in range (10):
+    time = i
+    timestepnumber = i
+    # create a time step
+    istep = TimeStep.TimeStep(time, dt, timestepnumber)
+    try:
+        serverApp.setProperty (Property.Property(i, PropertyID.PID_Concentration, ValueType.Scalar, i, None, 0))
+        serverApp.solveStep(istep)
+
+    except APIError.APIError as e:
+        print "Following API error occurred:",e
+        break
+
+print "done"
+prop = serverApp.getProperty(PropertyID.PID_CumulativeConcentration, i)
+print "Received ", prop.getValue(), " expected ", expectedValue
+if (prop.getValue() == expectedValue):
+    print "Test PASSED"
+else:
+    print "Test FAILED"
+
+serverApp.terminate();
+print "Ping test terminated"
