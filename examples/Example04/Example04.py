c318780bd8701c53da3007799f153306a5ce5725
Bořek Patzák <borek.patzak@gmail.com>
2014-09-09 11:59:48 +0200

    Added unstructured field cration example

---
+++ examples/Example04/Example04.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+sys.path.append('../..')
+
+from mupif import Field
+from mupif import FieldID
+from mupif import ValueType
+
+from mupif import Mesh
+from mupif import Vertex
+from mupif import Cell
+from mupif import BBox
+
+def main():
+
+    # generate field and corresponding mesh
+    mesh = Mesh.UnstructuredMesh()
+    vertices=[]
+    values = []
+    for i in range(40):
+        for j in range (15):
+            vertices.append(Vertex.Vertex((i*15)+j,(i*15)+j+1, (float(i),float(j),0.0)))
+            values.append((i,))
+    cells = []
+    num=0
+    for i in range(39):
+        for j in range (14):
+            cells.append(Cell.Quad_2d_lin(mesh, num, num, ((i*15)+j, (i+1)*15+j, (i+1)*15+j+1, ((i*15)+j+1))))
+            num=num+1
+
+    mesh.setup(vertices, cells)
+    field = Field.Field(mesh, FieldID.FID_Temperature, ValueType.Scalar, None, None, values)
+
+    # evaluate field at given point
+    position=(20., 7.5, 0.0)
+    value=field.evaluate(position)
+    print "Field value at position ", position, " is ", value
+
+
+
+
+
+if __name__ == '__main__':
+        main()
