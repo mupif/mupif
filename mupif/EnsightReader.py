880720c8b148daa6157b7e25717dc5b7384638f8
Bořek Patzák <borek.patzak@gmail.com>
2014-09-30 16:03:29 +0200

    Adding Example05: a skeleton for Celsian API

--- mupif/EnsightReader.py
+++ mupif/EnsightReader.py
@@ -4,136 +4,171 @@
 import sys
 sys.path.append('../')
 
-from mupif import *
 import vtk
-from vtk import *
 from mupif import Timer
+import numpy as np
+from mupif import Mesh
+from mupif import Vertex
+from mupif import Cell
+from mupif import Field
+from mupif import FieldID
+from mupif import ValueType
 
 #debug flag
 debug = 0
 
 class EnsightReader():
 
-	def __init__(self):
-		self.grid = None
-		#self.nx = 0 #numberOfNodesX
-		#self.ny = 0
-		#self.nz = 0 
-		#self.dim = 0 #dimension see mupif
-		return
+  def __init__(self):
+    self.grid = None
+    return
 
-	def readVtkFile(self, fileName):
-		self.reader = vtk.vtkUnstructuredGridReader()
-		self.reader.SetFileName(fileName)
-		self.reader.Update()
-		self.grid = self.reader.GetOutput()
-		self.points = self.grid.GetPoints()
+  def readVtkFile(self, fileName):
+    self.reader = vtk.vtkUnstructuredGridReader()
+    self.reader.SetFileName(fileName)
+    self.reader.Update()
+    self.grid = self.reader.GetOutput()
+    self.points = self.grid.GetPoints()
 
-	def readEnsightFile(self, fileName):
-		#read the ensight file
-		reader = vtk.vtkGenericEnSightReader()
-		reader.SetCaseFileName(fileName)
-		reader.Update()
+  def readEnsightFile(self, fileName):
+    #read the ensight file
+    reader = vtk.vtkGenericEnSightReader()
+    reader.SetCaseFileName(fileName)
+    reader.Update()
 
-		output = reader.GetOutput()
-		num_blocks =  output.GetNumberOfBlocks()	
-		#print "numb : ", num_blocks
+    output = reader.GetOutput()
+    num_blocks =  output.GetNumberOfBlocks()  
 
-		#blocks_unstructured is a list of objects of vtkUnstructuredGrid
-		blocks_unstructured = []
-		for i in range(num_blocks):
-			blocks_unstructured.append(output.GetBlock(i))
-		#print "blocks :", blocks_unstructured   
+    #blocks_unstructured is a list of objects of vtkUnstructuredGrid
+    blocks_unstructured = []
+    for i in range(num_blocks):
+      blocks_unstructured.append(output.GetBlock(i))
 
-		appendFilter = vtk.vtkAppendFilter()
-		i = 0
-		while i < len(blocks_unstructured):
-			appendFilter.AddInput(blocks_unstructured[i])
-			i=i+1
-		appendFilter.Update();
+    appendFilter = vtk.vtkAppendFilter()
+    i = 0
+    while i < len(blocks_unstructured):
+      appendFilter.AddInput(blocks_unstructured[i])
+      i=i+1
+    appendFilter.Update();
 
-		unstructuredGrid=vtk.vtkUnstructuredGrid()
-		unstructuredGrid.ShallowCopy(appendFilter.GetOutput());
-		w = vtk.vtkUnstructuredGridWriter()
-		w.SetInput(unstructuredGrid)
-		w.SetFileName(fileName+'.vtk')
-		w.Write()
-		self.readVtkFile(fileName+'.vtk')
+    unstructuredGrid=vtk.vtkUnstructuredGrid()
+    unstructuredGrid.ShallowCopy(appendFilter.GetOutput());
+    w = vtk.vtkUnstructuredGridWriter()
+    w.SetInput(unstructuredGrid)
+    w.SetFileName(fileName+'.vtk')
+    w.Write()
+    self.readVtkFile(fileName+'.vtk')
 
 
-	def readField (self, fileName, fieldName, componentID):
-		# Read the source file.
-		#self.reader = vtk.vtkUnstructuredGridReader()
-		#self.reader.SetFileName(fileName)
-		#self.reader.ReadAllScalarsOn()
-		#self.reader.Update()
+  def getMesh (self, cellFilter):
+    mesh = Mesh.UnstructuredMesh()
+    vertices=[]
+    coords = np.zeros((3), dtype='f')
+    for i in xrange(0, self.getNumberOfVertices()): 
+      coords=self.getCoords(i,coords)
+      tuple = (coords)
+      vertices.append(Vertex.Vertex(i,i+1, tuple))
 
-                uGrid = vtk.vtkUnstructuredGrid()
-		uGrid.ShallowCopy(self.reader.GetOutput())
-		#return uGrid.GetPointData().GetScalars(fieldName).GetValue( componentID )
-		return uGrid.GetCellData().GetScalars(fieldName).GetValue( componentID )
+      cells = []
+      for i in xrange(0, self.getNumberOfCells()):
+        if (self.giveCellType(i) == 12 and self.giveCellType(i) in cellFilter):
+          cells.append(Cell.Brick_3d_lin(mesh, i, i, (int(self.giveVertex(i,0)), int(self.giveVertex(i,1)), int(self.giveVertex(i,2)), int(self.giveVertex(i,3)), int(self.giveVertex(i,4)), int(self.giveVertex(i,5)), int(self.giveVertex(i,6)), int(self.giveVertex(i,7))) )) 
+        elif (self.giveCellType(i) == 9 and self.giveCellType(i) in cellFilter):
+          cells.append(Cell.Quad_2d_lin(mesh, i, i,(int(self.giveVertex(i,0)),int(self.giveVertex(i,1)),int(self.giveVertex(i,2)),int(self.giveVertex(i,3))) ))
+                        
+        mesh.setup(vertices, cells)
+        return mesh
 
+  def getField(self, mesh, fileName, fieldName, vertexBasedFlag, cellFilter):
+    values=[]
+    if (vertexBasedFlag == True):
+      for i in xrange(0, self.getNumberOfVertices()): 
+        values.append ((self.giveValueAtPoint(fieldName, i), ))
+    elif(vertexBasedFlag == False):
+      for i in xrange(0, self.getNumberOfCells()):
+        if (self.giveCellType(i) == 12 and self.giveCellType(i) in cellFilter):
+          values.append ((self.giveValueAtCell(fieldName, i), ))
+        elif (self.giveCellType(i) == 9 and self.giveCellType(i) in cellFilter):
+          values.append ((self.giveValueAtCell(fieldName, i), ))
+    return Field.Field(mesh, FieldID.FID_Temperature, ValueType.Scalar, None, None, values, Field.FieldType.FT_cellBased)
                 
-        def giveValueAtPoint(self, fieldName, componentID):
-		uGrid = vtk.vtkUnstructuredGrid()
-		uGrid.ShallowCopy(self.grid)
-		return uGrid.GetPointData().GetScalars(fieldName).GetValue( componentID )
+  def giveValueAtPoint(self, fieldName, componentID):
+    uGrid = vtk.vtkUnstructuredGrid()
+    uGrid.ShallowCopy(self.grid)
+    return uGrid.GetPointData().GetScalars(fieldName).GetValue( componentID )
 
-	def giveValueAtCell(self, fieldName, componentID):
-		uGrid = vtk.vtkUnstructuredGrid()
-		uGrid.ShallowCopy(self.grid)
-		return uGrid.GetCellData().GetScalars(fieldName).GetValue( componentID )
+  def giveValueAtCell(self, fieldName, componentID):
+    uGrid = vtk.vtkUnstructuredGrid()
+    uGrid.ShallowCopy(self.grid)
+    return uGrid.GetCellData().GetScalars(fieldName).GetValue( componentID )
 
-        def getNumberOfCells(self):
-		"""Returns the number of Cells."""
-		geomData = self.reader.GetOutput();
-		numCells = geomData.GetNumberOfCells()
-		print "number of cells : ", numCells
-		return numCells;
+  def giveVectorAtPoint(self, fieldName, i):
+    uGrid = vtk.vtkUnstructuredGrid()
+    uGrid.ShallowCopy(self.grid)
+    return uGrid.GetPointData().GetVectors(fieldName).GetTuple3(i)
 
-	def getNumberOfVertices(self):
-		geomData = self.reader.GetOutput();
-		numPts = geomData.GetNumberOfPoints()
-		print "number of points : ",numPts
-		return numPts;
+  def giveVectorAtCell(self, fieldName, i):
+    uGrid = vtk.vtkUnstructuredGrid()
+    uGrid.ShallowCopy(self.grid)
+    return uGrid.GetCellData().GetVectors(fieldName).GetTuple3(i)
 
-	def getCoords(self, i, coords):
-		#Get the xyz coordinate of the point
-		if debug:
-			with Timer.Timer() as t:
-				coords=self.points.GetPoint(i)
-			print('Request getCoords took %.03f sec.' % t.interval)
-		else:
-			coords=self.points.GetPoint(i)	
-		return coords
+  def giveTensorAtPoint(self, fieldName, i):
+    uGrid = vtk.vtkUnstructuredGrid()
+    uGrid.ShallowCopy(self.grid)
+    return uGrid.GetPointData().GetTensors(fieldName).GetTuple9(i)
 
-	def getBounds(self, b):
-		geomData = self.reader.GetOutput();
-		geomData.ComputeBounds()	
-		geomData.GetBounds(b)
-		return b
+  def giveTensorAtCell(self, fieldName, i):
+    uGrid = vtk.vtkUnstructuredGrid()
+    uGrid.ShallowCopy(self.grid)
+    return uGrid.GetCellData().GetTensors(fieldName).GetTuple9(i)
 
-	def giveCellDimension(self):
-		geomData = self.reader.GetOutput();
-		dim=geomData.GetCell(0).GetCellDimension()
-		print "dim :",dim 
-		return dim
+  def getNumberOfCells(self):
+    """Returns the number of Cells."""
+    geomData = self.reader.GetOutput();
+    numCells = geomData.GetNumberOfCells()
+    return numCells;
 
-	def giveCellType(self, i):
-		geomData = self.reader.GetOutput();
-		type=geomData.GetCellType(i)
-		#print "type of cells: ", type
-		return type
+  def getNumberOfVertices(self):
+    """Returns the number of Vertices."""
+    geomData = self.reader.GetOutput();
+    numPts = geomData.GetNumberOfPoints()
+    return numPts;
 
-	def giveNumberOfVertices(self, cellid):
-		geomData = self.reader.GetOutput();
-		PtIds = vtk.vtkIdList()	
-		geomData.GetCellPoints(cellid, PtIds)
-		return PtIds.GetNumberOfIds()
+  def getCoords(self, i, coords):
+    #Get the xyz coordinate of the point
+    if debug:
+      with Timer.Timer() as t:
+        coords=self.points.GetPoint(i)
+      print('Request getCoords took %.03f sec.' % t.interval)
+    else:
+      coords=self.points.GetPoint(i)  
+    return coords
 
-	def giveVertex(self, cellid, i):
-		geomData = self.reader.GetOutput();
-		PtIds = vtk.vtkIdList()	
-		geomData.GetCellPoints(cellid, PtIds)
-		ID = PtIds.GetId(i)
-		return ID
+  def getBounds(self, b):
+    geomData = self.reader.GetOutput();
+    geomData.ComputeBounds()  
+    geomData.GetBounds(b)
+    return b
+
+  def giveCellDimension(self, i):
+    geomData = self.reader.GetOutput();
+    dim=geomData.GetCell(i).GetCellDimension()
+    return dim
+
+  def giveCellType(self, i):
+    geomData = self.reader.GetOutput();
+    type=geomData.GetCellType(i)
+    return type
+
+  def giveNumberOfVertices(self, cellid):
+    geomData = self.reader.GetOutput();
+    PtIds = vtk.vtkIdList()  
+    geomData.GetCellPoints(cellid, PtIds)
+    return PtIds.GetNumberOfIds()
+
+  def giveVertex(self, cellid, i):
+    geomData = self.reader.GetOutput();
+    PtIds = vtk.vtkIdList()  
+    geomData.GetCellPoints(cellid, PtIds)
+    ID = PtIds.GetId(i)
+    return ID
