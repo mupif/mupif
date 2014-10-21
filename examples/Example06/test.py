1eb9e2ede7d4f1305ed4986eaa5f607da6615777
Bořek Patzák <borek.patzak@gmail.com>
2014-10-21 21:25:49 +0200

    Removing sshmode, sshTunnel unreliable, using ssh client to set up tunnel

--- examples/Example06/test.py
+++ examples/Example06/test.py
@@ -22,26 +22,10 @@ from mupif import PyroUtil
 import time as timeTime
 import Pyro4
 
-sshtmode = 1
-if (sshtmode == 0):
-    from sshtunnel import SSHTunnelForwarder
-
-    server = SSHTunnelForwarder(
-        ssh_address=('mech.fsv.cvut.cz', 22),
-        ssh_username="mmp",
-        ssh_password="mmp2014mmp",
-        remote_bind_address=('127.0.0.1', 44382),
-        local_bind_address=('127.0.0.1', 5555))
-
-    server.start()
-    print (server)
-    print("server.local_bind_port:%d "% server.local_bind_port)
-    timeTime.sleep(0.1)
-elif (sshtmode==1):
-    #Windows tunnel using putty
-    #tunnel = PyroUtil.sshTunnel(remoteHost='mech.fsv.cvut.cz', userName='mmp', localPort=5555, remotePort=44382, sshClient='putty', options='-i L:\.ssh\putty.ppk')
-    #Linux tunnel using ssh
-    tunnel = PyroUtil.sshTunnel(remoteHost='mech.fsv.cvut.cz', userName='mmp', localPort=5555, remotePort=44382, sshClient='ssh')
+#Windows tunnel using putty
+#tunnel = PyroUtil.sshTunnel(remoteHost='mech.fsv.cvut.cz', userName='mmp', localPort=5555, remotePort=44382, sshClient='putty', options='-i L:\.ssh\putty.ppk')
+#Linux tunnel using ssh
+tunnel = PyroUtil.sshTunnel(remoteHost='mech.fsv.cvut.cz', userName='mmp', localPort=5555, remotePort=44382, sshClient='ssh')
 
 time  = 0
 dt    = 1
@@ -53,8 +37,6 @@ ns     = PyroUtil.connectNameServer('mech.fsv.cvut.cz', 9090)
 
 # locate remote PingServer application, request remote proxy
 serverApp = PyroUtil.connectApp (ns, 'Mupif.PingServerApplication')
-
-#app2.__init__(None)
 
 try:
     appsig=serverApp.getApplicationSignature()
@@ -91,8 +73,5 @@ serverApp.terminate();
 logger.info("Time consumed %f s" % (timeTime.time()-start))
 logger.info("Ping test finished")
 
-if (sshtmode==0):
-    server.terminate()
-elif (sshtmode ==1):
-    tunnel.terminate()
+tunnel.terminate()
 
