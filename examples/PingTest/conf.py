f4bfaee55ef8edaac3e8248a5d3f8ceb7f55cb5e
Bořek Patzák <borek.patzak@gmail.com>
2014-10-30 15:37:27 +0100

    PingTest: further polisment

--- examples/PingTest/conf.py
+++ examples/PingTest/conf.py
@@ -1,7 +1,7 @@
 # List of job applications - server details
 # Do not use a dictionary - items are in arbitrary order
 # The format is (name, serverName, username, localNATPort, serverPort,sshClient)
-apps=[('local','localhost', 'smilauer', 5554, 44380, 'manual'),
+apps=[('local','localhost', 'username', 44380, 44380, 'manual'),
       ('celsian','jaja.fsv.cvut.cz', 'bp', 5555, 44381,'ssh'),
       ('micress','jaja.fsv.cvut.cz', 'bp', 5556, 44382,'ssh')]
 
@@ -22,3 +22,17 @@ appIndx_UserName = 2
 appIndx_NATPort = 3
 appIndx_RemotePort = 4
 appIndx_SshClient = 5
+
+import logging
+#put logging before Pyro4 module
+logging.basicConfig(filename='mupif.pyro.log',filemode='w',datefmt="%Y-%m-%d %H:%M:%S",level=logging.DEBUG)
+logging.getLogger('Pyro4').setLevel(logging.INFO)
+logger = logging.getLogger('test.py')
+logger.setLevel(logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler()) #display logging also on screen
+
+import Pyro4
+Pyro4.config.SERIALIZER="pickle"
+Pyro4.config.PICKLE_PROTOCOL_VERSION=2 #to work with python 2.x and 3.x
+Pyro4.config.SERIALIZERS_ACCEPTED={'pickle'}
+
--- examples/PingTest/test.py
+++ examples/PingTest/test.py
@@ -3,32 +3,20 @@ sys.path.append('../..')
 import os
 os.environ['PYRO_HMAC_KEY'] = "mmp-secret-key" #do not change 
 
-import logging
-#put logging before Pyro4 module
-logging.basicConfig(filename='mupif.pyro.log',filemode='w',datefmt="%Y-%m-%d %H:%M:%S",level=logging.DEBUG)
-logging.getLogger('Pyro4').setLevel(logging.DEBUG)
-logger = logging.getLogger('test.py')
-logger.setLevel(logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler()) #display logging also on screen
-
 from mupif import Application
 from mupif import APIError
 from mupif import PyroUtil
 import time as timeTime
 import conf
 
-import Pyro4
-Pyro4.config.SERIALIZER="pickle"
-Pyro4.config.PICKLE_PROTOCOL_VERSION=2 #to work with python 2.x and 3.x
-Pyro4.config.SERIALIZERS_ACCEPTED={'pickle'}
-
-
 start = timeTime.time()
 #locate nameserver
 ns     = PyroUtil.connectNameServer('mech.fsv.cvut.cz', 9090)
 
+results=[]
 for apprecord in conf.apps:
-    logging.info("Trying to connect to server " + str(apprecord[conf.appIndx_Name]))
+    starti = timeTime.time()
+    conf.logger.info("Trying to connect to server " + str(apprecord[conf.appIndx_Name]))
     tunnel = PyroUtil.sshTunnel(remoteHost=apprecord[conf.appIndx_ServerName], userName=apprecord[conf.appIndx_UserName], 
                                 localPort=apprecord[conf.appIndx_NATPort], remotePort=apprecord[conf.appIndx_RemotePort],
                                 sshClient=apprecord[conf.appIndx_SshClient])
@@ -37,12 +25,21 @@ for apprecord in conf.apps:
     app = PyroUtil.connectApp(ns, PyroUtil.getNSAppName(conf.jobname, apprecord[conf.appIndx_Name]))
     if app:
         appsig=app.getApplicationSignature()
-        logging.info("Successfully connected to " + appsig)
-        logging.info("Time since the beginning of ping test consumed %f s" % (timeTime.time()-start) )
+        msg = "Successfully connected to %-30s (%4.2f s)"%(appsig, timeTime.time()-starti)
+        conf.logger.info(msg)
+        conf.logger.info("Time elapsed %f s" % (timeTime.time()-starti) )
     else:
-        logging.error("Unable to connect to " + str(apprecord[conf.appIndx_Name]) )
+        msg = "Unable to connect to " + str(apprecord[conf.appIndx_Name])
+        conf.logger.error(msg)
     if tunnel:
         tunnel.terminate()
+    results.append(msg)
+
+
+print ("=========SUMMARY============")
+for r in results:
+    print (r)
+
 
 print ("Total time consumed %f s" % (timeTime.time()-start))
 print ("Ping test finished")
