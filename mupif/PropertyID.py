2545f716303de6425ad2f3ab37b541394e4571a8
Bořek Patzák <borek.patzak@gmail.com>
2014-11-08 13:47:01 +0100

    Adding some property names for demo purposes

--- mupif/PropertyID.py
+++ mupif/PropertyID.py
@@ -5,3 +5,7 @@ Module defining PropertyID as enumeration
 PID_Concentration = 1
 PID_CumulativeConcentration = 2
 
+PID_Demo_Min = 9990
+PID_Demo_Max = 9991
+PID_Demo_Integral = 9992
+PID_Demo_Volume = 9993
