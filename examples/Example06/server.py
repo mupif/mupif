9aec5d58d6bf5afa2a51d62353fd1d82ad95b4b0
Vít Šmilauer <smilauer@cml.fsv.cvut.cz>
2014-11-24 15:34:45 +0100

    Change of iptable's address.

--- examples/Example06/server.py
+++ examples/Example06/server.py
@@ -17,14 +17,14 @@ hkey = 'mmp-secret-key'
 
 # required firewall settings (on ubuntu):
 # for computer running daemon (this script)
-# sudo iptables -A INPUT -p tcp -d 0/0 -s 0/0 --dport 44382 -j ACCEPT
+# sudo iptables -A INPUT -p tcp -d 0/0 -s 0/0 --dport 44361 -j ACCEPT
 # for computer running a nameserver
 # sudo iptables -A INPUT -p tcp -d 0/0 -s 0/0 --dport 9090 -j ACCEPT
 
 
 class PingServerApplication(Application.Application):
     """
-    Simple application that computes an arithmetical average of mapped property
+    Simple application that computes an aritmetical average of a mapped property
     """
     def __init__(self, file):
         self.value = 0.0
