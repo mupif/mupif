b14f489590d2a3aca75a1a5bd52fecab678e51c0
Bořek Patzák <borek.patzak@gmail.com>
2015-06-16 22:34:31 +0200

    Added mechanical solver to Example10 (works in local scenario Demo03.local.py)

--- examples/Example10/Demo03.local.py
+++ examples/Example10/Demo03.local.py
@@ -11,10 +11,21 @@ from mupif import Mesh
 from mupif import Vertex
 from mupif import Cell
 
-app = demoapp.thermal('input.in')
-print app.getApplicationSignature()
 
-sol = app.solveStep(TimeStep.TimeStep(0,1)) 
-f = app.getField(FieldID.FID_Temperature, 0.0)
-data = f.field2VTKData().tofile('example2')
+if True:
+    app = demoapp.thermal('input.in','.')
+    print app.getApplicationSignature()
+
+    sol = app.solveStep(TimeStep.TimeStep(0,1)) 
+    f = app.getField(FieldID.FID_Temperature, 0.0)
+    data = f.field2VTKData().tofile('example2')
+
+if True:
+    app2 = demoapp.mechanical('inputm.in', '.')
+    print app2.getApplicationSignature()
+
+    app2.setField(f)
+    sol = app2.solveStep(TimeStep.TimeStep(0,1)) 
+    f = app2.getField(FieldID.FID_Displacement, 0.0)
+    data = f.field2VTKData().tofile('example3')
 
