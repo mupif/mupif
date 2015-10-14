fd137363caf744f7abf1e751caee5971b97c5cd6
eudoxos <eu@doxos.eu>
2015-10-14 12:17:54 +0200

    travis: remove -oPermitTTY=no since unsupported by older OpenSSH

--- mupif/examples/ssh/test_ssh_server.sh
+++ mupif/examples/ssh/test_ssh_server.sh
@@ -2,5 +2,6 @@
 # http://stackoverflow.com/a/246128/761090
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-/usr/sbin/sshd -d -f /dev/null -oPort=2024 -oProtocol=2 -oListenAddress=127.0.0.1 -oForceCommand=/sbin/nologin -oPermitTTY=no -oHostKey=$DIR/test_ssh_server_rsa_key -oAuthorizedKeysFile=$DIR/test_ssh_client_rsa_key.pub -oUsePrivilegeSeparation=no -oPubkeyAuthentication=yes -oPasswordAuthentication=no
+# removed -oPermitTTY=no since unsupported on older openssh servers (travis in particular, ubuntu 14.04LTS)
+/usr/sbin/sshd -d -f /dev/null -oPort=2024 -oProtocol=2 -oListenAddress=127.0.0.1 -oForceCommand=/sbin/nologin -oHostKey=$DIR/test_ssh_server_rsa_key -oAuthorizedKeysFile=$DIR/test_ssh_client_rsa_key.pub -oUsePrivilegeSeparation=no -oPubkeyAuthentication=yes -oPasswordAuthentication=no
 
