612b48ecb2ec962a59c20508d63ecc51394033f3
Bořek Patzák <borek.patzak@gmail.com>
2016-06-10 10:30:12 +0200

    Added PyroFile.setBuffSize method allowing to set buffer size

--- mupif/PyroFile.py
+++ mupif/PyroFile.py
@@ -1,23 +1,23 @@
-# 
-#           MuPIF: Multi-Physics Integration Framework 
+#
+#           MuPIF: Multi-Physics Integration Framework
 #               Copyright (C) 2010-2014 Borek Patzak
-# 
+#
 #    Czech Technical University, Faculty of Civil Engineering,
 #  Department of Structural Mechanics, 166 29 Prague, Czech Republic
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA  02110-1301  USA
 #
 
@@ -29,7 +29,7 @@ class PyroFile (object):
     def __init__ (self, filename, mode, buffsize=1024):
         """
         Constructor. Opens the corresponding file handle.
-        
+
         :param str filename: file name to open
         :param str mode: file mode ("r" opens for reading, "w" opens for writting, "rb" read binary, "rw" write binary)
         :param int buffsize: optional size of file byte chunk that is to be transferred
@@ -51,10 +51,17 @@ class PyroFile (object):
     def setChunk (self, buffer):
         """
         Writes the given chunk of data into the file, which should be opened in write mode.
-        
-        :param str buffer: data chunk to append 
+
+        :param str buffer: data chunk to append
         """
         self.myfile.write(buffer)
+
+    def setBuffSize (self, buffSize):
+        """
+        Allows to set the receiver buffer size.
+        :param int buffSize: new buffer size
+        """
+        self.buffsize = buffSize
 
     def close (self):
         """
