946a91422ef88db633c9f66bc8363e7f33e55ee2
Bořek Patzák <borek.patzak@gmail.com>
2017-03-21 12:51:28 +0100

    Example16 converted to runJobManagerServer

--- mupif/examples/Example16-thermoMechanicalNonStat-VPN-JobMan/thermalServer.py
+++ mupif/examples/Example16-thermoMechanicalNonStat-VPN-JobMan/thermalServer.py
@@ -9,16 +9,11 @@ import conf_vpn as cfg
 #locate nameserver
 ns = PyroUtil.connectNameServer(nshost=cfg.nshost, nsport=cfg.nsport, hkey=cfg.hkey)
 
-#Run a daemon for jobManager on this machine
-daemon = cfg.Pyro4.Daemon(host=cfg.server, port=cfg.serverPort)
 #Run job manager on a server
-
 jobMan = JobManager.SimpleJobManager2(daemon, ns, None, cfg.jobManName, cfg.portsForJobs, cfg.jobManWorkDir, os.getcwd(), 'thermalServerConfig', cfg.jobMan2CmdPath, cfg.maxJobs, cfg.socketApps)
 
-#set up daemon with JobManager
-uri = daemon.register(jobMan)
-#register JobManager to nameServer
-ns.register(cfg.jobManName, uri)
-print ("Daemon for JobManager runs at " + str(uri))
-#waits for requests
-daemon.requestLoop()
+PyroUtil.runJobManagerServer(server=cfg.server, port=cfg.serverPort, nathost=cfg.server, natport=cfg.serverPort, nshost=cfg.nshost, nsport=cfg.nsport, 
+                             nsname=cfg.jobManName, hkey=cfg.hkey, jobman=jobMan)
+
+
+
