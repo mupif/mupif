aa93c0795e78458fdbe6955b490dfbb0256043af
Bořek Patzák <borek.patzak@gmail.com>
2017-03-21 12:54:40 +0100

    Example16 fixing daemon initialization

--- mupif/examples/Example16-thermoMechanicalNonStat-VPN-JobMan/thermalServer.py
+++ mupif/examples/Example16-thermoMechanicalNonStat-VPN-JobMan/thermalServer.py
@@ -9,11 +9,14 @@ import conf_vpn as cfg
 #locate nameserver
 ns = PyroUtil.connectNameServer(nshost=cfg.nshost, nsport=cfg.nsport, hkey=cfg.hkey)
 
+#Run a daemon for jobManager on this machine
+daemon = cfg.Pyro4.Daemon(host=cfg.server, port=cfg.serverPort)
+
 #Run job manager on a server
 jobMan = JobManager.SimpleJobManager2(daemon, ns, None, cfg.jobManName, cfg.portsForJobs, cfg.jobManWorkDir, os.getcwd(), 'thermalServerConfig', cfg.jobMan2CmdPath, cfg.maxJobs, cfg.socketApps)
 
 PyroUtil.runJobManagerServer(server=cfg.server, port=cfg.serverPort, nathost=cfg.server, natport=cfg.serverPort, nshost=cfg.nshost, nsport=cfg.nsport, 
-                             nsname=cfg.jobManName, hkey=cfg.hkey, jobman=jobMan)
+                             nsname=cfg.jobManName, hkey=cfg.hkey, jobman=jobMan, daemon=daemon)
 
 
 
