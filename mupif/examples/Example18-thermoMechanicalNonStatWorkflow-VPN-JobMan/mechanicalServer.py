b7f0565e0d8cbef01e2505a6c47e0239dc917c4f
Bořek Patzák <borek.patzak@gmail.com>
2017-04-21 08:56:00 +0200

    Example18 mechanical server using runAppServer method

--- mupif/examples/Example18-thermoMechanicalNonStatWorkflow-VPN-JobMan/mechanicalServer.py
+++ mupif/examples/Example18-thermoMechanicalNonStatWorkflow-VPN-JobMan/mechanicalServer.py
@@ -11,11 +11,18 @@ import conf_vpn as cfg
 ns = PyroUtil.connectNameServer(nshost=cfg.nshost, nsport=cfg.nsport, hkey=cfg.hkey)
 
 #Run a daemon. It will run even the port has DROP/REJECT status. The connection from a client is then impossible.
-daemon = cfg.Pyro4.Daemon(host=cfg.server3, port=cfg.serverPort3)
+#daemon = cfg.Pyro4.Daemon(host=cfg.server3, port=cfg.serverPort3)
 
 mechanical = demoapp.mechanical('..'+os.path.sep+'Example13-thermoMechanicalNonStat'+os.path.sep+'inputM13.in', '.')
+
+
+PyroUtil.runAppServer (server=cfg.server3, port=cfg.serverPort3, natport='', nathost='',
+                       nshost=cfg.nshost, nsport=cfg.nsport,
+                       nsname='mechanical', hkey=cfg.hkey, app=mechanical)
+
+
 #register agent
-uri = daemon.register(mechanical)
-ns.register('mechanical', uri)
-mupif.log.debug ("Daemon for mechanical problem runs at " + str(uri))
-daemon.requestLoop()
+#uri = daemon.register(mechanical)
+#ns.register('mechanical', uri)
+#mupif.log.debug ("Daemon for mechanical problem runs at " + str(uri))
+#daemon.requestLoop()
