b6c64e7c4cfc8be6dc024dc82ded53f35788eb61
Bořek Patzák <borek.patzak@gmail.com>
2017-04-26 13:58:51 +0200

    Updating Example02

--- mupif/examples/Example02/client.py
+++ mupif/examples/Example02/client.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-mode = 1 #Communication type 1=local(default), 2=ssh tunnel, 3=VPN
+mode = 2 #Communication type 1=local(default), 2=ssh tunnel, 3=VPN
 import os, sys
 sys.path.append('..')
 if mode==3:
@@ -37,8 +37,10 @@ time = 0
 timestepnumber=0
 targetTime = 1.3
 
-if mode==2: #just print out how to set up a SSH tunnel
-    PyroUtil.sshTunnel(cfg.server, cfg.serverUserName, cfg.serverNatport, cfg.serverPort, 'manual', cfg.options)
+sshContext = None
+if mode==1: #just print out how to set up a SSH tunnel
+    sshContext =  PyroUtil.SSHContext(userName=cfg.serverUserName, sshClient='ssh', options=cfg.options)
+    #PyroUtil.sshTunnel(cfg.server, cfg.serverUserName, cfg.serverNatport, cfg.serverPort, 'ssh', cfg.options)
 
 #locate nameserver
 ns = PyroUtil.connectNameServer(cfg.nshost, cfg.nsport, cfg.hkey)
@@ -46,7 +48,7 @@ ns = PyroUtil.connectNameServer(cfg.nshost, cfg.nsport, cfg.hkey)
 # application1 is local, create its instance
 app1 = application1(None)
 # application2 is remote, request remote proxy
-app2=PyroUtil.connectApp(ns, cfg.appName)
+app2=PyroUtil.connectApp(ns, cfg.appName, sshContext)
 
 while (abs(time -targetTime) > 1.e-6):
     #determine critical time step
