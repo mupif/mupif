c13cb9a69d36fdebafd72be9af3aeade3f2e1315
Bořek Patzák <borek.patzak@gmail.com>
2017-04-27 17:54:00 +0200

    adding missing Demo13.py

---
+++ mupif/examples/Example13-thermoMechanicalNonStat/Demo13.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env pythonoun            
+from __future__ import print_function
+import sys
+sys.path.append('../../..')
+from mupif import *
+from mupif import log as logger
+# Import module Example10/demoapp.py
+sys.path.append('../Example10')
+import demoapp
+
+time  = 0.
+dt = 0.
+timestepnumber = 0
+targetTime = 3
+
+thermal = demoapp.thermal_nonstat('inputT13.in','.')
+mechanical = demoapp.mechanical('inputM13.in', '.')
+matPlotFig = None
+
+while (abs(time - targetTime) > 1.e-6):
+
+    logger.debug("Step: %g %g %g"%(timestepnumber,time,dt))
+    # create a time step
+    istep = TimeStep.TimeStep(time, dt, timestepnumber)
+
+    try:
+        # solve problem 1
+        thermal.solveStep(istep)
+        # request Temperature from thermal
+        ft = thermal.getField(FieldID.FID_Temperature, istep.getTime())
+        #print ("T(l/2)=", f.evaluate((2.5,0.2,0.0)))
+        #data = ft.field2VTKData().tofile('T_%s'%str(timestepnumber))
+        #matPlotFig = ft.field2Image2D(title='Thermal '+str(time), barRange=(0,9), fileName='thermal.png', matPlotFig=matPlotFig)
+
+        mechanical.setField(ft)
+        sol = mechanical.solveStep(istep) 
+        f = mechanical.getField(FieldID.FID_Displacement, istep.getTime())
+        #print ("D(l,1)=", f.evaluate((5.0,1.0,0.0)))
+        data = f.field2VTKData().tofile('M_%s'%str(timestepnumber))
+        matPlotFig = f.field2Image2D(title='Mechanical ' + str(time), barRange=(-9e-5, 1.6e-6), fileName='mechanical.png', fieldComponent=1, figsize = (12,6), matPlotFig=matPlotFig) 
+
+        # finish step
+        thermal.finishStep(istep)
+        mechanical.finishStep(istep)
+
+        # determine critical time step
+        dt = min (thermal.getCriticalTimeStep(), mechanical.getCriticalTimeStep())
+
+        # update time
+        time = time+dt
+        if (time > targetTime):
+            # make sure we reach targetTime at the end
+            time = targetTime
+        timestepnumber = timestepnumber+1
+
+    except APIError.APIError as e:
+        logger.error("Following API error occurred:",e)
+        break
+
+# terminate
+thermal.terminate();
+mechanical.terminate();
+ft.field2Image2DBlock() #To block the window
