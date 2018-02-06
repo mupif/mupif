3f97769d2bb8fc5b6c38113c3d516ba2431da00e
Martin Horak <nitramkaroh@seznam.cz>
2018-02-06 16:21:05 +0100

    test of travis

--- mupif/Field.py
+++ mupif/Field.py
@@ -19,7 +19,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA  02110-1301  USA
-#
+# huhu
 from __future__ import print_function
 from builtins import range
 from builtins import object
