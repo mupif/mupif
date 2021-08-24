0cf2b8827a8335024c0ca61a493729d07e66edd9
Stanislav <sulcstanda@seznam.cz>
2021-08-25 00:34:29 +0200

    Added Units to LookupTable

--- examples/Example12/lookuptable_test.py
+++ examples/Example12/lookuptable_test.py
@@ -1,17 +1,17 @@
 import sys
 sys.path.extend(['..', '../..'])
 import mupif as mp
+from mupif import U
 
 
 if __name__ == '__main__':
-    lut = mp.MemoryLookupTable()
-    lut.setData([
+    lut = mp.MemoryLookupTable(data=[
         [0., 1., 2., 3.],
         [1., 2., 3., 4.],
         [2., 3., 4., 5.]
-    ])
+    ], units=[U.s, U.m, U.mm, U.kg])
 
-    print(lut.evaluate([3., 4., 5.]))  # produces 2.
-    print(lut.evaluate([0., 1., 2.]))  # produces None
-    print(lut.evaluate([1., 2., 3.]))  # produces 0.
-    print(lut.evaluate([2., 3., 4.]))  # produces 1.
+    print(lut.evaluate([3.*U.m, 4.*U.mm, 5.*U.kg]))  # produces 2. s
+    print(lut.evaluate([0.*U.m, 1.*U.mm, 2.*U.kg]))  # produces None
+    print(lut.evaluate([1.*U.m, 2.*U.mm, 3.*U.kg]))  # produces 0. s
+    print(lut.evaluate([2.*U.m, 3.*U.mm, 4.*U.kg]))  # produces 1. s
--- mupif/lookuptable.py
+++ mupif/lookuptable.py
@@ -3,8 +3,8 @@ import Pyro5.api
 import typing
 import pydantic
 
-
 from .units import Quantity
+from .units import Unit
 
 
 @Pyro5.api.expose
@@ -39,13 +39,17 @@ class MemoryLookupTable(LookupTable):
     """
 
     data: typing.List[typing.List[float]] = []  # first column stores the values, the rest of the columns is used to store the parameters
+    units: typing.List[Unit] = []  # first element stores unit of the value, the rest of the elements stores units of the particular parameters
     default_value: float = None
     tolerance: float = 0.0001
 
     def __init__(self, *, metadata={}, **kw):
         super().__init__(metadata=metadata, **kw)
 
-    def setData(self, data):
+        self.n_records = len(self.data)
+        self.n_params = len(self.data[0])-1
+
+    def _setData(self, data):
         if len(data):
             len_check = len(data[0])
             for record in data:
@@ -56,18 +60,17 @@ class MemoryLookupTable(LookupTable):
     def evaluate(self, params):
         """
         Returns the value for given parameters.
-
+        :param: Quantity[] params:
         :rtype: Quantity
         """
-        len_record = len(self.data[0])-1
-        if len(params) != len_record:
+        if len(params) != self.n_params:
             raise ValueError("Unexpected number of given parameters.")
         for record in self.data:
             match = True
-            for j in range(0, len_record):
-                if params[j] < record[j+1]-self.tolerance or params[j] > record[j+1]+self.tolerance:
+            for j in range(0, self.n_params):
+                if params[j].inUnitsOf(self.units[j+1]).value < record[j+1]-self.tolerance or params[j].inUnitsOf(self.units[j+1]).value > record[j+1]+self.tolerance:
                     match = False
                     break
             if match is True:
-                return record[0]
+                return Quantity(record[0], self.units[0])
         return self.default_value
