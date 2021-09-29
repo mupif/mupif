e8367558a012c88ed322b219f9e6ccc704cf3d1c
Stanislav <sulcstanda@seznam.cz>
2021-09-29 02:24:44 +0200

    Update of example 11

--- examples/Example11/workflow1.py
+++ examples/Example11/workflow1.py
@@ -6,10 +6,9 @@ import logging
 log = logging.getLogger()
 
 from model1 import Model1
-from model2 import Model2
 
 
-class Example11(mp.workflow.Workflow):
+class Example11_1(mp.workflow.Workflow):
    
     def __init__(self, metadata={}):
         """
@@ -31,13 +30,11 @@ class Example11(mp.workflow.Workflow):
 
         # model references
         self.m1 = None
-        self.m2 = None
     
     def initialize(self, workdir='', targetTime=0*mp.U.s, metadata={}, validateMetaData=True):
     
         super().initialize(workdir=workdir, targetTime=targetTime, metadata=metadata, validateMetaData=validateMetaData)
         self.m1 = Model1()
-        self.m2 = Model2()
 
         # To be sure update only required passed metadata in models
         passingMD = {
@@ -49,7 +46,6 @@ class Example11(mp.workflow.Workflow):
         }
 
         self.m1.initialize(metadata=passingMD)
-        self.m2.initialize(metadata=passingMD)
 
     def solveStep(self, istep, stageID=0, runInBackground=False):
         
@@ -57,40 +53,32 @@ class Example11(mp.workflow.Workflow):
         log.debug("Step: %g %g %g" % (istep.getTime().getValue(), istep.getTimeIncrement().getValue(), istep.number))
 
         try:
-            # solve problem 1
             self.m1.solveStep(istep)
-            # handshake the data
-            grainState = self.m1.get(mp.DataID.PID_GrainState, self.m1.getAssemblyTime(istep))
-            self.m2.set(grainState)
-            self.m2.solveStep(istep)
-            grainState2 = self.m2.get(mp.DataID.PID_GrainState, self.m1.getAssemblyTime(istep))
+            grainState_output = self.m1.get(mp.DataID.PID_GrainState, istep.getTime())
+            grainState_output.cloneHandle('./data1.h5')
 
         except mp.apierror.APIError as e:
             log.error("Following API error occurred: %s" % e)
 
         self.m1.finishStep(istep)
-        self.m2.finishStep(istep)
 
     def getCriticalTimeStep(self):
-        # determine critical time step
-        return min(self.m1.getCriticalTimeStep(), self.m2.getCriticalTimeStep())
+        return self.m1.getCriticalTimeStep()
 
     def terminate(self):
         if self.m1 is not None:
             self.m1.terminate()
-        if self.m2 is not None:
-            self.m2.terminate()
         super().terminate()
     
     def getApplicationSignature(self):
-        return "Example11 workflow 1.0"
+        return "Example11 workflow1 1.0"
 
     def getAPIVersion(self):
         return "1.0"
 
 
 if __name__ == '__main__':
-    workflow = Example11()
+    workflow = Example11_1()
     workflowMD = {
         'Execution': {
             'ID': '1',
