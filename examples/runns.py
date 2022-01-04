a7629cc24140382177eaf572190963be341aadf4
stanislavsulc <sulcstanda@seznam.cz>
2022-01-04 22:28:18 +0100

    Update of testing nameserver script

--- examples/runns.py
+++ examples/runns.py
@@ -1,39 +1,15 @@
-#!/usr/bin/env -S python3 -u
-import subprocess
 import os
 import sys
-import time
-import atexit
 import logging
-thisDir = os.path.dirname(os.path.abspath(__file__))+'/..'
+thisDir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(thisDir+'/..')
+import mupif as mp
 logging.basicConfig(format='%(message)s')
 log = logging.getLogger('run-ex')
 log.setLevel(logging.DEBUG)
 
 
-def getExec():
-    return tuple([sys.executable, '-u'])
-
-
-def runBg(sleep=1):
-    env = os.environ.copy()
-    env['PYTHONPATH'] = os.pathsep.join([thisDir])
-    bbg = [
-        subprocess.Popen([*getExec(), thisDir+'/tools/nameserver.py'], bufsize=0, env=env)
-    ]
-    time.sleep(sleep)
-
-    def bbgTerminate():
-        for b in bbg:
-            b.terminate()
-        time.sleep(.5)
-        for b in bbg:
-            if b.returncode is None:
-                b.kill()
-
-    atexit.register(bbgTerminate)
-
-
 if __name__ == "__main__":
-    runBg()
+    nshost, nsport = mp.pyroutil.runNameserverBg()
+    log.warning(f"Starting nameserver on {nshost}:{nsport}")
     input("Press key to shutdown nameserver.")
