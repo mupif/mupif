f5368afedc0bb0df30f0abb45fa0ff834dbe947e
stanislavsulc <sulcstanda@seznam.cz>
2022-02-10 02:52:34 +0100

    Json schema validation o ValueType presence for Type mupif.Property

--- examples/Example13/main.py
+++ examples/Example13/main.py
@@ -23,13 +23,13 @@ class Workflow13(mp.workflow.Workflow):
             "Description": "Calculates multiplication of two given values using a simple model",
             "Inputs": [
                 {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Value_1', "Obj_ID": '1',
-                 'Description': 'Input value 1', 'Units': 's', 'Required': True, "Set_at": "timestep"},
+                 'Description': 'Input value 1', 'Units': 's', 'Required': True, "Set_at": "timestep", "ValueType": "Scalar"},
                 {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Value_2', "Obj_ID": '2',
-                 'Description': 'Input value 2', 'Units': 's', 'Required': True, "Set_at": "timestep"}
+                 'Description': 'Input value 2', 'Units': 's', 'Required': True, "Set_at": "timestep", "ValueType": "Scalar"}
             ],
             "Outputs": [
                 {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Multiplication_result',
-                 'Description': 'Result of multiplication', 'Units': 's^2'}
+                 'Description': 'Result of multiplication', 'Units': 's^2', "ValueType": "Scalar"}
             ],
         }
         mp.workflow.Workflow.__init__(self, metadata=MD)
