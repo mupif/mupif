a0a68953d021200afdfd14a542f0e92fd04e23ce
Václav Šmilauer <eu@doxos.eu>
2022-02-17 09:19:33 +0100

    add JobManager.getModelMetadata()

--- mupif/tests/testApp.py
+++ mupif/tests/testApp.py
@@ -5,5 +5,8 @@ import Pyro5
 
 @Pyro5.api.expose
 class testApp (mupif.model.Model):
+    def __init__(self,metadata=None):
+        if metadata is None: metadata={'Name':'Test Application','ID':'mupif-tests-testApp'}
+        super().__init__(metadata=metadata)
     def solveStep(self, tstep, stageID=0, runInBackground=False):
         time.sleep(60*10)
