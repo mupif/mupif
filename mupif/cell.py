8a7155c99f76fc78fa474c8bc64be1afcdd72569
Václav Šmilauer <eu@doxos.eu>
2024-06-03 11:00:12 +0200

    pydantic v2: use global module attribute rather than class attribute for subtype cache in Cell

--- mupif/cell.py
+++ mupif/cell.py
@@ -38,6 +38,9 @@ debug = 0
 # in element tolerance
 tolerance = 0.001
 
+# cache
+_cell_subclasses = {}
+
 
 @Pyro5.api.expose
 class Cell(baredata.BareData):
@@ -78,7 +81,8 @@ class Cell(baredata.BareData):
         """
         Return class object (not instance) for given cell geometry type. Does introspection of all subclasses of Cell caches the result.
         """
-        if not Cell._subclasses:
+        global _cell_subclasses
+        if not _cell_subclasses:
             # cache all subclasses (recursive)
             def get_subclasses(cls):
                 ret = []
@@ -87,8 +91,8 @@ class Cell(baredata.BareData):
                     ret.extend(get_subclasses(sc))
                 return ret
             for sc in get_subclasses(Cell):
-                Cell._subclasses[sc.getGeometryType()] = sc
-        return Cell._subclasses[cgt]
+                _cell_subclasses[sc.getGeometryType()] = sc
+        return _cell_subclasses[cgt]
 
     def copy(self):
         """
