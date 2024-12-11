e454cfb94d4a03d29e380fd36fa9aa3a562bc2ad
Václav Šmilauer <eu@doxos.eu>
2024-12-11 12:38:33 +0100

    Use numpydantic.dtype instead of obsolete nptyping

--- mupif/ndtypes.py
+++ mupif/ndtypes.py
@@ -1,26 +1,24 @@
 import warnings
-with warnings.catch_warnings():
-    from numpydantic import NDArray, Shape
-    warnings.simplefilter("ignore")
-    import nptyping
+from numpydantic import NDArray, Shape
+from numpydantic.dtype import Float, Unicode
 
-NumpyArray=NDArray[Shape['*, ...'],nptyping.Floating]
-NumpyArrayStr=NDArray[Shape['*, ...'],nptyping.Unicode]
-NDArr1=NDArray[Shape["1"],nptyping.Floating]
-NDArr2=NDArray[Shape["2"],nptyping.Floating]
-NDArr2xX=NDArray[Shape["3,*"],nptyping.Floating]
-NDArr3=NDArray[Shape["3"],nptyping.Floating]
-NDArr4=NDArray[Shape["3"],nptyping.Floating]
-NDArr6=NDArray[Shape["6"],nptyping.Floating]
-NDArr6x2=NDArray[Shape["6,2"],nptyping.Floating]
-NDArr8=NDArray[Shape["8"],nptyping.Floating]
-NDArr3xX=NDArray[Shape["3,*"],nptyping.Floating]
-NDArr123=NDArray[Shape["1-3"],nptyping.Floating]
-NDArr23=NDArray[Shape["2-3"],nptyping.Floating]
-NDArr123xX=NDArray[Shape['1-3,*'],nptyping.Floating]
-NDArr123x3=NDArray[Shape['1-3,3'],nptyping.Floating]
+NumpyArray=NDArray[Shape['*, ...'],Float]
+NumpyArrayStr=NDArray[Shape['*, ...'],Unicode]
+NDArr1=NDArray[Shape["1"],Float]
+NDArr2=NDArray[Shape["2"],Float]
+NDArr2xX=NDArray[Shape["3,*"],Float]
+NDArr3=NDArray[Shape["3"],Float]
+NDArr4=NDArray[Shape["3"],Float]
+NDArr6=NDArray[Shape["6"],Float]
+NDArr6x2=NDArray[Shape["6,2"],Float]
+NDArr8=NDArray[Shape["8"],Float]
+NDArr3xX=NDArray[Shape["3,*"],Float]
+NDArr123=NDArray[Shape["1-3"],Float]
+NDArr23=NDArray[Shape["2-3"],Float]
+NDArr123xX=NDArray[Shape['1-3,*'],Float]
+NDArr123x3=NDArray[Shape['1-3,3'],Float]
 # for vertex values type over elements (the 1-3 is for value type, but should be more generic for tensors etc)
-NDArr3x123=NDArray[Shape['3,1-3'],nptyping.Floating]
-NDArr4x123=NDArray[Shape['4,1-3'],nptyping.Floating]
-NDArr6x123=NDArray[Shape['6,1-3'],nptyping.Floating]
-NDArr8x123=NDArray[Shape['8,1-3'],nptyping.Floating]
+NDArr3x123=NDArray[Shape['3,1-3'],Float]
+NDArr4x123=NDArray[Shape['4,1-3'],Float]
+NDArr6x123=NDArray[Shape['6,1-3'],Float]
+NDArr8x123=NDArray[Shape['8,1-3'],Float]
