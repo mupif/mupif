439a31e1700dd33c26548be5c8da8be7f9ddc218
Stanislav Šulc <sulcstanda@seznam.cz>
2026-04-29 22:21:04 +0200

    to_db_dict return type

--- mupif/dbrec.py
+++ mupif/dbrec.py
@@ -1,17 +1,24 @@
 from __future__ import annotations
 from .baredata import BareData
-from typing import Union,Literal,Optional
+from typing import Union, Literal, Optional, Any
 import pydantic
 
 class DbDictable(BareData):
     def to_db_dict_impl(self): raise NotImplementedError(f'Classes derived from DbDictable must implement to_db_dict_impl. (class {self.__class__.__name__})')
 
     @pydantic.validate_call
-    def to_db_dict(self,dialect:Optional[Literal['edm']]=None):
-        ret=self.to_db_dict_impl()
-        edmRepl={'Unit':'unit','Value':'value'}
-        if dialect=='edm': return dict([(edmRepl.get(k,k),v) for k,v in ret.items()])
-        else: return ret
+    def to_db_dict(self, dialect: Optional[Literal['edm']] = None) -> dict[str, Any]:
+        ret = self.to_db_dict_impl()
+        edm_repl = {'Unit': 'unit', 'Value': 'value'}
+        if dialect == 'edm':
+            return {edm_repl.get(k, k): v for k, v in ret.items()}  # type: ignore
+        return ret
+    
+    # def to_db_dict(self,dialect:Optional[Literal['edm']]=None):
+    #     ret=self.to_db_dict_impl()
+    #     edmRepl={'Unit':'unit','Value':'value'}
+    #     if dialect=='edm': return dict([(edmRepl.get(k,k),v) for k,v in ret.items()])
+    #     else: return ret
 
     @staticmethod
     @pydantic.validate_call
